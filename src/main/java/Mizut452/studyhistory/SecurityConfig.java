84d6e2c8dd545afa01ade42aadbfc612af085b80
M1zut4 <ssvror444@gmail.com>
2022-10-31 01:36:56 +0900

    SecurityConfig.javaの作成

---
+++ src/main/java/Mizut452/studyhistory/SecurityConfig.java
@@ -0,0 +1,54 @@
+package Mizut452.studyhistory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    private static final String USER_SQL ="SELECT username FROM userlist WHERE username=?";
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin( login -> login
+                .loginProcessingUrl("/login")
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error")
+                .permitAll()
+        ).logout(logout -> logout
+                .logoutSuccessUrl("/")
+
+        ).authorizeHttpRequests(authz -> authz
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .permitAll()
+                .mvcMatchers("/")
+                .permitAll()
+                .mvcMatchers("/login")
+                .permitAll()
+                .mvcMatchers("/createaccount")
+                .permitAll()
+                .anyRequest().authenticated()
+        );
+        http.csrf().disable();
+        return http.build();
+
+    }
+
+}
